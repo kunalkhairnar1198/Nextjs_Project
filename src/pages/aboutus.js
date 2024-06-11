c5ab5b8f9077876773c0bfdc1c4340bb08ef6cde
kunal khairnar <kunalkhairnar61@gmail.com>
2024-06-11 18:43:21 +0530

    firstCommit

---
+++ src/pages/aboutus.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutusPage = () => {
+  return (
+    <>
+        <h1>About us</h1>
+    </>
+  )
+}
+
+export default AboutusPage
---
+++ src/pages/news.js
@@ -0,0 +1,12 @@
+// our-domain.com/news
+
+const NewsPage = () => {
+  return (
+    <h1>
+      the newsjs file
+    </h1>
+  )
+}
+
+export default NewsPage;
+
